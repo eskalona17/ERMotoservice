f8607f312816f2f127f8b48beb64e1411be41a3b
eskalona17 <fer.escalonso@gmail.com>
2024-03-10 17:27:14 +0100

    server routes

--- server.js
+++ server.js
@@ -8,10 +8,23 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-// Sirve los archivos estáticos desde la carpeta 'dist'
+// Sirve archivos estáticos desde la carpeta 'dist'
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Redirige todas las solicitudes a tu archivo HTML principal
+// Maneja solicitudes a rutas específicas
+app.get('/politica-privacidad', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'politica-privacidad.html'));
+});
+
+app.get('/aviso-legal', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'aviso-legal.html'));
+});
+
+app.get('/politica-cookies', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'politica-cookies.html'));
+});
+
+// Redirige todas las demás solicitudes a tu archivo HTML principal
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
