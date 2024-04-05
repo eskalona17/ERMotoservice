bd975ee5f4d0bab60a9afcb572c84cc334da2b00
eskalona17 <fer.escalonso@gmail.com>
2024-04-05 16:20:56 +0200

    add intersection observer in services card

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,31 +1,89 @@
-  import React from "react"
-  import ServicesItem from "../services/ServicesItem"
-  import { data } from "../../assets/data"
-  import { LuShieldCheck } from "react-icons/lu"
-  import { FaRegHandshake } from "react-icons/fa6"
-  import { PiTruck } from "react-icons/pi"
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import ServicesItem from "../services/ServicesItem";
+import { data } from "../../assets/data";
+import { LuShieldCheck } from "react-icons/lu";
+import { FaRegHandshake } from "react-icons/fa6";
+import { PiTruck } from "react-icons/pi";
 
-  const Services = () => {
-    return (
-      <section id="services" className="custom-screen flex flex-row overflow-x-auto lg:justify-center">
-        {data.map((item) => (
-          <ServicesItem key={item.id} {...item} icon={renderIcon(item.icon, "#215a83", 40)}/>
-        ))}
-      </section>
-    )
-  }
+const Services = () => {
+  const targetRefs = useRef([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleIntersection = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const index = targetRefs.current.indexOf(entry.target);
+        setActiveIndex(index);
+      }
+    });
+  };
+
+  const observerOptions = useMemo(() => {
+    return {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5,
+    };
+  }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      handleIntersection,
+      observerOptions
+    );
+
+    targetRefs.current.forEach((target) => {
+      if (target) observer.observe(target);
+    });
+
+    return () => {
+      targetRefs.current.forEach((target) => {
+        if (target) observer.unobserve(target);
+      });
+    };
+  }, [observerOptions]);
 
   const renderIcon = (icon, color, size) => {
     switch (icon) {
       case "security":
-        return <LuShieldCheck style={{ color, fontSize: size }}/>
+        return <LuShieldCheck style={{ color, fontSize: size }} />;
       case "care":
-        return <FaRegHandshake style={{ color, fontSize: size }}/>
+        return <FaRegHandshake style={{ color, fontSize: size }} />;
       case "experience":
-        return <PiTruck style={{ color, fontSize: size }}/>
+        return <PiTruck style={{ color, fontSize: size }} />;
       default:
-        return null
+        return null;
     }
-  }
+  };
 
-  export default Services
+  return (
+    <section
+      id="services"
+      className="custom-screen flex flex-col lg:justify-center"
+    >
+      <div className="flex flex-row overflow-x-auto pb-4">
+        {data.map((item, index) => (
+          <ServicesItem
+            key={item.id}
+            ref={(el) => (targetRefs.current[index] = el)}
+            {...item}
+            icon={renderIcon(item.icon, "#215a83", 40)}
+          />
+        ))}
+      </div>
+
+      <div className="pt-4 space-x-2 flex justify-center">
+        {data.map((_, index) => (
+          <span
+            key={index}
+            className={`dot w-3 h-3 rounded-full ${
+              index === activeIndex ? "bg-blue-600" : "bg-gray-400"
+            }`}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
--- src/components/services/ServicesItem.jsx
+++ src/components/services/ServicesItem.jsx
@@ -1,13 +1,13 @@
-import React from "react"
+import React, { forwardRef } from "react";
 
-const ServicesItem = ({ icon, title, description }) => {
+const ServicesItem = forwardRef(({ icon, title, description, ...restProps }, ref) => {
   return (
-    <article className="flex-shrink-0 w-72 lg:w-80 bg-white rounded-lg p-6 mx-2 lg:mx-4 shadow-md">
+    <article ref={ref} className="flex-shrink-0 w-72 lg:w-80 bg-white rounded-lg p-6 mx-2 lg:mx-4 shadow-md" {...restProps}>
       <span className="flex items-center justify-center text-3xl mb-4">{icon}</span>
       <h3 className="text-primary-700 text-xl font-bold mb-2 text-center">{title}</h3>
       <p className="text-secondary-500 p-2 text-center">{description}</p>
     </article>
-  )
-}
+  );
+});
 
-export default ServicesItem
+export default ServicesItem;
