17fb08528ce6f722b7560f1b8c9ab56dca3c68d7
eskalona17 <fer.escalonso@gmail.com>
2024-09-01 20:27:20 +0200

    add fix in footer for small screens

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,9 @@
-import React from "react"
-import { LuCopyright } from "react-icons/lu"
-import { Link } from "react-router-dom"
+import React from "react";
+import { LuCopyright } from "react-icons/lu";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear();
 
   return (
     <section
@@ -11,9 +11,9 @@ const Footer = () => {
       className="bg-gradient-to-b from-primary-300 via-primary-400 to-primary-600"
     >
       <div className="max-w-screen-xl mx-auto py-12 flex flex-col justify-center items-center text-secondary-50">
-        <div className="flex w-full items-center gap-y-5 flex-col">
+        <div className="flex w-full items-center gap-y-5 flex-col text-center">
           <div className="flex flex-col w-full items-center md:items-center justify-center gap-x-5 md:gap-x-20">
-          <div className="flex space-x-5 mb-5">
+            <div className="flex space-x-5 mb-5">
               <Link to="/aviso-legal">Aviso Legal</Link>
               <Link to="/politica-cookies">Política de Cookies</Link>
               <Link to="/politica-privacidad">Política de Privacidad</Link>
@@ -25,22 +25,23 @@ const Footer = () => {
               <p className="">{currentYear} ERMotoservice</p>
             </div>
           </div>
-          <p className="font-semibold">
-          Made with ❤️ by{" "}
-          <a
-            href="https://www.linkedin.com/in/fernandoescalonalonso/"
-            rel="nofollow noopener noreferrer"
-            target="_blank"
-            className="cursor-pointer focus:outline-none"
-          >
-            Fernando Escalona
-          </a>
-        </p>
+          <div className="flex flex-col md:flex-row md:gap-x-2">
+            <p className="font-semibold">
+              Made with ❤️ by
+            </p>
+              <a
+                href="https://www.linkedin.com/in/fernandoescalonalonso/"
+                rel="nofollow noopener noreferrer"
+                target="_blank"
+                className="cursor-pointer focus:outline-none"
+              >
+                Fernando Escalona
+              </a>
+          </div>
         </div>
-      
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
